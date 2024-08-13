8d858fc72daab099d3b8c249d0a855ab8b16db0d
Aleksandr <ponomarenko.100299@gmail.com>
2024-08-13 11:45:59 +0300

    readExpression func

--- calculator.go
+++ calculator.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	expression, err := readExpression()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(expression)
+}
+
+func readExpression() ([]string, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inputString := strings.Fields(scanner.Text())
+	if len(inputString) != 3 {
+		err := errors.New("the format of a mathematical operation does not satisfy the condition - two operands and one operator (+, -, /, *).")
+		return inputString, err
+	}
+	return inputString, nil
 }
